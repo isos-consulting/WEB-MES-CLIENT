296904a15a484654127a45f9bc2d91da07c2e835
ybr <br.yoon@kisos.net>
2021-11-02 09:48:32 +0900

    권한 없는 사용자 로그인 오류  수정 관련해서 푸쉬할때 누락된 파일

--- src/components/UI/side-navbar/side-navbar.ui.recoil.ts
+++ src/components/UI/side-navbar/side-navbar.ui.recoil.ts
@@ -3,11 +3,11 @@ import { atom } from "recoil";
 /** 사이드 네비게이션 바 메뉴 recoil atom */
 export const atSideNavMenuContent = atom({
   key: 'atSideNavMenuContent',
-  default: {}
+  default: {},
 });
 
 /** 사이드 네비게이션 바 메뉴 recoil atom */
-export const atSideNavMenuRawData = atom({
+export const atSideNavMenuRawData = atom<any[]>({
   key: 'atSideNavMenuRawData',
-  default: []
+  default: null,
 });
