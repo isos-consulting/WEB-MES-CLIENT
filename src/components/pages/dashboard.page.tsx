2bc89d23f40c393fdba88694ece99e8fa51c709f
shs <hs.seo@kisos.net>
2021-11-09 17:52:17 +0900

    [화면 추가]  - 대시보드 화면 추가

---
+++ src/components/pages/dashboard.page.tsx
@@ -0,0 +1,25 @@
+
+import { Col, Row } from 'antd'
+import React from 'react'
+import { PieGraph } from '../UI'
+
+export const Dashboard= () => {
+  return (
+    <div>
+      <Row gutter={[16,16]}>
+        <Col span={6}>
+          {/* <PieGraph /> */}
+        </Col>
+        <Col span={6}>
+        zz
+        </Col>
+        <Col span={6}>
+        zz
+        </Col>
+        <Col span={6}>
+        zz
+        </Col>
+      </Row>
+    </div>
+  )
+}
