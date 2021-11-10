26c76936968610220f13cfaf749695a8bb2722f5
ybr <br.yoon@kisos.net>
2021-11-10 15:56:58 +0900

    [코드 보완] 1. antd locale 한글화한 부분 코드 간략하게 수정함

--- src/index.tsx
+++ src/index.tsx
@@ -1,37 +1,22 @@
 import React, { lazy, Suspense } from "react";
 import ReactDOM from "react-dom";
-// import { RecoilRoot } from "recoil";
-// import { ThemeProvider } from "styled-components";
 import './index.css';
 import "antd/dist/antd.css";
 import koKr from "antd/lib/locale-provider/ko_KR";
-import "dayjs/locale/ko";
-
-// import App from "./app";
+import "dayjs/locale/ko"
 import { GlobalStyles } from "./styles/theme.style";
 
 const App = lazy(() => import('./app'));
 const RecoilRoot = lazy(() => import('recoil').then(module => ({default: module.RecoilRoot})));
 const ConfigProvider = lazy(() => import('antd').then(module => ({default: module.ConfigProvider})));
 
-const locale = koKr.DatePicker;
-const lang = koKr.DatePicker.lang;
-const convLocale = {
-  ...koKr,
-  DatePicker: {
-    ...locale,
-    lang: {
-      ...lang,
-      shortWeekDays: ['일', '월', '화', '수', '목', '금', '토'],
-      shortMonths: ['1월', '2월', '3월', '4월', '5월', '6월', '7월', '8월', '9월', '10월', '11월', '12월'],
-    },
-  }
-}
+koKr.DatePicker.lang.shortWeekDays = ['일', '월', '화', '수', '목', '금', '토'];
+koKr.DatePicker.lang.shortMonths = ['1월', '2월', '3월', '4월', '5월', '6월', '7월', '8월', '9월', '10월', '11월', '12월'];
 
 ReactDOM.render(
   <Suspense fallback='...loading'>
   <RecoilRoot>
-    <ConfigProvider locale={convLocale}>
+    <ConfigProvider locale={koKr}>
       <GlobalStyles />
       <App />
     </ConfigProvider>
