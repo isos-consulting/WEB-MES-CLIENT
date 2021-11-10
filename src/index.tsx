6ea1812b7dea9da8bc794b8edb0b76eed6970411
ybr <br.yoon@kisos.net>
2021-11-10 15:34:30 +0900

    날짜 선택기 한글화

--- src/index.tsx
+++ src/index.tsx
@@ -9,16 +9,29 @@ import "dayjs/locale/ko";
 
 // import App from "./app";
 import { GlobalStyles } from "./styles/theme.style";
-// import { ConfigProvider } from "antd";
 
 const App = lazy(() => import('./app'));
 const RecoilRoot = lazy(() => import('recoil').then(module => ({default: module.RecoilRoot})));
 const ConfigProvider = lazy(() => import('antd').then(module => ({default: module.ConfigProvider})));
 
+const locale = koKr.DatePicker;
+const lang = koKr.DatePicker.lang;
+const convLocale = {
+  ...koKr,
+  DatePicker: {
+    ...locale,
+    lang: {
+      ...lang,
+      shortWeekDays: ['일', '월', '화', '수', '목', '금', '토'],
+      shortMonths: ['1월', '2월', '3월', '4월', '5월', '6월', '7월', '8월', '9월', '10월', '11월', '12월'],
+    },
+  }
+}
+
 ReactDOM.render(
   <Suspense fallback='...loading'>
   <RecoilRoot>
-    <ConfigProvider locale={koKr}>
+    <ConfigProvider locale={convLocale}>
       <GlobalStyles />
       <App />
     </ConfigProvider>
