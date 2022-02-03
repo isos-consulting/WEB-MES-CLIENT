a19f6316290b7caabdd2d909a4a4a6e10fd1942f
kkh <kh.kim@kisos.net>
2022-02-03 16:55:06 +0900

    ic-147 검사처리유형 화면 추가

--- src/functions/comm.function.ts
+++ src/functions/comm.function.ts
@@ -294,6 +294,21 @@ export const getMenus = async () => {
             update_fg: true
           },
           {
+            component_nm: 'PgAdmInspHandlingType',
+            create_fg: true,
+            delete_fg: true,
+            first_menu_uuid: 'adm',
+            icon: null,
+            lv: 2,
+            menu_nm: '검사처리유형',
+            menu_type: 'page',
+            menu_uri: '/adm/insp-handling-type',
+            menu_uuid: 'admInspHandlingType',
+            read_fg: true,
+            sub_menu: [],
+            update_fg: true
+          },
+          {
             component_nm: 'PgAdmCycleUnit',
             create_fg: true,
             delete_fg: true,
