c694b0e688cd423781f6831674a239a7345516d3
shs <godppun@naver.com>
2022-02-04 09:11:34 +0900

    hotfix .. add nav icon and uncin convert page fix

--- src/images/nav.index.ts
+++ src/images/nav.index.ts
@@ -11,6 +11,9 @@ import ico_nav_production from './navigation/ico_production_outline_white.png';
 import ico_nav_qualityManagementSystems from './navigation/ico_qms_outline_white.png';
 import ico_nav_sales from './navigation/ico_product_outline_white.png';
 import ico_nav_standard from './navigation/ico_basic_outline_white.png';
+import ico_nav_equipment from './navigation/ico_nav_equipment.png';
+import ico_nav_mold from './navigation/ico_nav_mold.png';
+import ico_nav_kpi from './navigation/ico_nav_kpi.png';
 
 
 
@@ -28,4 +31,7 @@ export {
   ico_nav_qualityManagementSystems, 
   ico_nav_sales, 
   ico_nav_standard, 
+  ico_nav_equipment,
+  ico_nav_mold,
+  ico_nav_kpi,
 };
