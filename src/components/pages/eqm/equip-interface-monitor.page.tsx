3375ae5c29a12750ef7bc3fbda4e253d9a3ee888
rjh <jh.ra@kisos.net>
2022-04-18 17:03:29 +0900

    설비 인터페이스 모니터링 화면 추가

---
+++ src/components/pages/eqm/equip-interface-monitor.page.tsx
@@ -0,0 +1,24 @@
+import React from 'react';
+
+export const PgEquiInterfaceMonitor = () => {
+    return (
+        <>
+            <div>
+                <span>설비1</span>
+                <div> 대기 </div>
+                <span>설비1</span>
+            </div>
+            <div>
+                <span>설비1</span>
+                <div> 작업중 </div>
+                <span>설비1</span>
+            </div>
+            <div>
+                <span>설비1</span>
+                <div> 비가동 </div>
+                <span>설비1</span>
+            </div>
+            설비 가동 현황 페이지
+        </>
+    )
+}
