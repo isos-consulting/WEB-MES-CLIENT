145b6d3f0d52b3a225e6b1fef30f17930db417ff
rjh <jh.ra@kisos.net>
2022-04-19 11:32:38 +0900

    더미 데이터 생성하여 설비 인터페이스 화면에서 설비 정보가 동적으로 생성되는지 검증

--- src/components/pages/eqm/equip-interface-monitor.page.tsx
+++ src/components/pages/eqm/equip-interface-monitor.page.tsx
@@ -1,41 +1,77 @@
-import React from 'react';
+import React, { useState, useEffect } from "react";
+import {
+  ITpMonitorProps,
+  TpMonitor,
+} from "~/components/templates/monitoring/monitoring.template";
+import { IMonitorProps } from "~/components/UI/card/monitor.ui";
+
+const dummyEquips: IMonitorProps[] = [
+  {
+    title: "설비1",
+    content: "55%",
+    footer: "가동 8h | 목표 12h",
+    status: "Run",
+  },
+  {
+    title: "설비2",
+    content: "",
+    footer: "",
+    status: "Stop",
+  },
+  {
+    title: "설비3",
+    content: "",
+    footer: "",
+    status: "Downtime",
+  },
+];
+
+const searchNewEquips: IMonitorProps[] = [
+  {
+    title: "설비1",
+    content: "55%",
+    footer: "가동 8h | 목표 12h",
+    status: "Run",
+  },
+  {
+    title: "설비2",
+    content: "55%",
+    footer: "가동 8h | 목표 12h",
+    status: "Run",
+  },
+  {
+    title: "설비3",
+    content: "",
+    footer: "",
+    status: "Run",
+  },
+  {
+    title: "설비4",
+    content: "",
+    footer: "",
+    status: "Stop",
+  },
+];
 
 export const PgEquiInterfaceMonitor = () => {
-    return (
-        <>
-            <div style={{width: '300px', backgroundColor: '#ffffff', border: 'none'}}>
-                <div style={{display:'flex', justifyContent:'flex-start', height:'60px'}}>
-                <span style={{fontSize: '15px', marginLeft:'15px', marginTop:'20px'}}>설비1</span>
-                </div>
-                <div style={{display: 'flex', justifyContent:'space-around'}}>
-                <div style={{display: 'flex', justifyContent:'center', alignItems:'center', width: '70px', height:'70px', border: '1px solid #6ad198', borderRadius: '50%', backgroundColor: '#00af4e', color: '#ffffff'}}>55%</div>
-                </div>
-                <div style={{height:'70px',display:'flex',justifyContent:'center',alignItems:'center'}}>
-                <span>가동 8.5h | 목표 12h</span>
-                </div>
-            </div>
-            <div style={{width: '300px', backgroundColor: '#ffffff', border: 'none'}}>
-                <div style={{display:'flex', justifyContent:'flex-start', height:'60px'}}>
-                <span style={{fontSize: '15px', marginLeft:'15px', marginTop:'20px'}}>설비1</span>
-                </div>
-                <div style={{display: 'flex', justifyContent:'space-around'}}>
-                <div style={{display: 'flex', justifyContent:'center', alignItems:'center', width: '70px', height:'70px', border: '1px solid #fee491', borderRadius: '50%', backgroundColor: '#fcc002', color: '#ffffff'}}>55%</div>
-                </div>
-                <div style={{height:'70px',display:'flex',justifyContent:'center',alignItems:'center'}}>
-                <span>가동 8.5h | 목표 12h</span>
-                </div>
-            </div>
-            <div style={{width: '300px', backgroundColor: '#ffffff', border: 'none'}}>
-                <div style={{display:'flex', justifyContent:'flex-start', height:'60px'}}>
-                <span style={{fontSize: '15px', marginLeft:'15px', marginTop:'20px'}}>설비1</span>
-                </div>
-                <div style={{display: 'flex', justifyContent:'space-around'}}>
-                <div style={{display: 'flex', justifyContent:'center', alignItems:'center', width: '70px', height:'70px', border: '1px solid #fe9292', borderRadius: '50%', backgroundColor: '#fe0201', color: '#ffffff'}}>55%</div>
-                </div>
-                <div style={{height:'70px',display:'flex',justifyContent:'center',alignItems:'center'}}>
-                <span>가동 8.5h | 목표 12h</span>
-                </div>
-            </div>
-        </>
-    )
-}
+  const [equipStatus, setEquipStatus] = useState<IMonitorProps[]>([]);
+
+  const monitorProps: ITpMonitorProps = {
+    equips: equipStatus,
+  };
+
+  const handleSearchButtonClick = () => {
+    setEquipStatus(searchNewEquips);
+  };
+
+  useEffect(() => {
+    setEquipStatus(dummyEquips);
+  }, []);
+
+  return (
+    <>
+      <button onClick={handleSearchButtonClick}> 조회하기 </button>
+      <TpMonitor {...monitorProps} />
+    </>
+  );
+};
