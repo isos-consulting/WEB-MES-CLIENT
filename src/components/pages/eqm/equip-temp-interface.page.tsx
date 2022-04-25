019704a32e25b75fdcdb977fa46ff20f7d142e87
rjh <jh.ra@kisos.net>
2022-04-25 18:49:40 +0900

    api 요청 자료를 그래프 자료로 변환

--- src/components/pages/eqm/equip-temp-interface.page.tsx
+++ src/components/pages/eqm/equip-temp-interface.page.tsx
@@ -1,3 +1,4 @@
+import dayjs from "dayjs";
 import { chain } from "lodash";
 import React, { useState } from "react";
 import { Container, LineGraph, Searchbox, useSearchbox } from "~/components/UI";
@@ -7,229 +8,7 @@ enum SensorColorPalette {
     TEMP1 = 'hsl(336, 70%, 50%)',
     TEMP2 = 'hsl(26, 70%, 50%)'
 }
-
-const dummyData = [
-    {
-      id: "TEMP1",
-      color: SensorColorPalette.TEMP1,
-      data: [
-        {
-          x: "2022-04-25 00:00:00",
-          y: 159,
-        },
-        {
-          x: "2022-04-25 01:00:00",
-          y: 222,
-        },
-        {
-          x: "2022-04-25 02:00:00",
-          y: 165,
-        },
-        {
-          x: "2022-04-25 03:00:00",
-          y: 210,
-        },
-        {
-          x: "2022-04-25 04:00:00",
-          y: 45,
-        },
-        {
-          x: "2022-04-25 05:00:00",
-          y: 176,
-        },
-        {
-          x: "2022-04-25 06:00:00",
-          y: 89,
-        },
-        {
-          x: "2022-04-25 07:00:00",
-          y: 70,
-        },
-        {
-          x: "2022-04-25 08:00:00",
-          y: 256,
-        },
-        {
-          x: "2022-04-25 09:00:00",
-          y: 252,
-        },
-        {
-          x: "2022-04-25 10:00:00",
-          y: 83,
-        },
-        {
-          x: "2022-04-25 11:00:00",
-          y: 14,
-        },
-      ],
-    },
-    {
-      id: "TEMP2",
-      color: SensorColorPalette.TEMP2,
-      data: [
-        {
-          x: "2022-04-25 00:00:00",
-          y: 100,
-        },
-        {
-          x: "2022-04-25 01:00:00",
-          y: 252,
-        },
-        {
-          x: "2022-04-25 02:00:00",
-          y: 25,
-        },
-        {
-          x: "2022-04-25 03:00:00",
-          y: 20,
-        },
-        {
-          x: "2022-04-25 04:00:00",
-          y: 54,
-        },
-        {
-          x: "2022-04-25 05:00:00",
-          y: 167,
-        },
-        {
-          x: "2022-04-25 06:00:00",
-          y: 70,
-        },
-        {
-          x: "2022-04-25 07:00:00",
-          y: 171,
-        },
-        {
-          x: "2022-04-25 08:00:00",
-          y: 265,
-        },
-        {
-          x: "2022-04-25 09:00:00",
-          y: 225,
-        },
-        {
-          x: "2022-04-25 10:00:00",
-          y: 200,
-        },
-        {
-          x: "2022-04-25 11:00:00",
-          y: 83,
-        },
-      ],
-    },
-  ];
   
-  let searchData = [
-    {
-      id: "TEMP1",
-      color: SensorColorPalette.TEMP1,
-      data: [
-        {
-          x: "2022-04-25 00:00:00",
-          y: 19,
-        },
-        {
-          x: "2022-04-25 01:00:00",
-          y: 222,
-        },
-        {
-          x: "2022-04-25 02:00:00",
-          y: 156,
-        },
-        {
-          x: "2022-04-25 03:00:00",
-          y: 120,
-        },
-        {
-          x: "2022-04-25 04:00:00",
-          y: 54,
-        },
-        {
-          x: "2022-04-25 05:00:00",
-          y: 167,
-        },
-        {
-          x: "2022-04-25 06:00:00",
-          y: 98,
-        },
-        {
-          x: "2022-04-25 07:00:00",
-          y: 7,
-        },
-        {
-          x: "2022-04-25 08:00:00",
-          y: 256,
-        },
-        {
-          x: "2022-04-25 09:00:00",
-          y: 252,
-        },
-        {
-          x: "2022-04-25 10:00:00",
-          y: 83,
-        },
-        {
-          x: "2022-04-25 11:00:00",
-          y: 14,
-        },
-      ],
-    },
-    {
-      id: "TEMP2",
-      color: SensorColorPalette.TEMP2,
-      data: [
-        {
-          x: "2022-04-25 00:00:00",
-          y: 100,
-        },
-        {
-          x: "2022-04-25 01:00:00",
-          y: 22,
-        },
-        {
-          x: "2022-04-25 02:00:00",
-          y: 256,
-        },
-        {
-          x: "2022-04-25 03:00:00",
-          y: 102,
-        },
-        {
-          x: "2022-04-25 04:00:00",
-          y: 45,
-        },
-        {
-          x: "2022-04-25 05:00:00",
-          y: 176,
-        },
-        {
-          x: "2022-04-25 06:00:00",
-          y: 89,
-        },
-        {
-          x: "2022-04-25 07:00:00",
-          y: 70,
-        },
-        {
-          x: "2022-04-25 08:00:00",
-          y: 256,
-        },
-        {
-          x: "2022-04-25 09:00:00",
-          y: 252,
-        },
-        {
-          x: "2022-04-25 10:00:00",
-          y: 83,
-        },
-        {
-          x: "2022-04-25 11:00:00",
-          y: 14,
-        },
-      ],
-    },
-  ];
-
 interface InterfaceApiResponse {
   success: boolean;
   state_cd: string;
@@ -244,7 +23,7 @@ interface RawDatas {
 }
 
 interface EquipApiDataType {
-  reg_date: string;
+  created_at: string;
   data_map_id: number;
   data_map_nm: string;
   value: number;
@@ -262,70 +41,10 @@ interface GraphProps {
   data: AxisDataType[]
 }
 
-
-
-const tempAPI = async() => {
-  const apiResponse: InterfaceApiResponse = {
-    "success": true,
-    "state_cd": "gatDataHistory-S-0000",
-    "state": {
-        "state_tag": "gatDataHistory",
-        "type": "SUCCESS",
-        "state_no": "0000"
-    },
-    "message": "데이터 조회 성공",
-    "datas": {
-        "value": {
-            "count": 0
-        },
-        "raws":
-        [
-          {
-            reg_date: "2022-04-25 10:00:00",
-            data_map_id: 1,
-            data_map_nm: 'TEMP1',
-            value: 83
-          },
-          {
-            reg_date: "2022-04-25 12:00:00",
-            data_map_id: 1,
-            data_map_nm: 'TEMP1',
-            value: 26
-          },
-          {
-            reg_date: "2022-04-25 14:00:00",
-            data_map_id: 1,
-            data_map_nm: 'TEMP1',
-            value: 26
-          },
-          {
-            reg_date: "2022-04-25 10:00:00",
-            data_map_id: 2,
-            data_map_nm: 'TEMP2',
-            value: 14
-          },
-          {
-            reg_date: "2022-04-25 12:00:00",
-            data_map_id: 2,
-            data_map_nm: 'TEMP2',
-            value: 40
-          },
-          {
-            reg_date: "2022-04-25 13:00:00",
-            data_map_id: 2,
-            data_map_nm: 'TEMP2',
-            value: 40
-          },
-        ]
-    }
-  }
-  return apiResponse;
-};
-
-const convertToAxis = (datas:RawDatas) => {
-  return datas.raws.map(raw => {
+const convertToAxis = (raws:EquipApiDataType[]) => {
+  return raws.map(raw => {
     return {
-      x: raw.reg_date,
+      x: raw.created_at,
       y: raw.value,
       id: raw.data_map_nm,
     }
@@ -339,7 +58,7 @@ const groupingRaws = (raws: AxisDataType[], key: string) => {
 }
 
 interface EqmTempSearchCondition {
-    reg_date: string;
+    created_at: string;
     end_date: string;
     temperature: string;
 }
@@ -350,8 +69,14 @@ export const PgEqmTempInterface = () => {
   const [graph, setGraph] = useState(initialData);
 
   const handleSearchButtonClick = async (searchPayLoads: EqmTempSearchCondition)=>{
-    console.log(getData(searchPayLoads, 'gat/data-history/report'));
-    const { datas } = await tempAPI();
+    const value = dayjs(searchPayLoads.end_date);
+    const endDate = value.format();
+    const startDate = value.subtract(1, 'hour').format();
+
+    const datas = await getData({
+      start_date: startDate,
+      end_date: endDate,
+    }, 'gat/data-history/report');
     const axis = convertToAxis(datas);
     const group = groupingRaws(axis, "id");
 
@@ -360,8 +85,8 @@ export const PgEqmTempInterface = () => {
   };
 
   const { props } = useSearchbox("SEARCH_INPUTBOX", [
-    {type: "datetime", id: "reg_date", label: "조회일시", disabled: false, default: getNow()},
-    {type: "datetime", id: "end_date", disabled: false, default: getNow() },
+    // {type: "datetime", id: "start_date", label: "조회일시", disabled: false, default: getNow()},
+    {type: "datetime", id: "end_date", label: "조회일시", disabled: false, default: getNow() },
     {type: "text", id: "temperature", label: "온도", disabled: false, default: "",}
   ]);
 
