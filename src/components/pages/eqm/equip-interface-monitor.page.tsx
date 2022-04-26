c86a163241b6782775a3e2c03eed5630b5846110
rjh <jh.ra@kisos.net>
2022-04-26 11:07:57 +0900

    설비 인터페이스 현황 API 호출

--- src/components/pages/eqm/equip-interface-monitor.page.tsx
+++ src/components/pages/eqm/equip-interface-monitor.page.tsx
@@ -1,6 +1,7 @@
 import React, { useState, useEffect } from "react";
 import { IDefineMonitorProps } from "~/components/UI/card/monitor.ui";
 import { MonitorCards, ITpMonitorCardsProps } from "~/components/UI/card/monitors.ui";
+import { getData } from "~/functions";
 
 export const PgEqmInterfaceMonitor = () => {
   const [equipStatus, setEquipStatus] = useState<IDefineMonitorProps[]>([]);
@@ -9,12 +10,12 @@ export const PgEqmInterfaceMonitor = () => {
     equips: equipStatus,
   };
 
-  const handleSearchButtonClick = () => {
-    setEquipStatus([]);
+  const handleSearchButtonClick = async () => {
+    setEquipStatus(await getData({}, 'std/data-map/report') ?? []);
   };
 
   useEffect(() => {
-    setEquipStatus([]);
+    handleSearchButtonClick();
   }, []);
 
   return (
