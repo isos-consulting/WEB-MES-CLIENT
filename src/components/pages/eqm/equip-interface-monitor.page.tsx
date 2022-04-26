529aec5ae9bd0ee2c2fdf681b086d52b5a51998e
rjh <jh.ra@kisos.net>
2022-04-26 10:35:17 +0900

    더미 데이터 제거

--- src/components/pages/eqm/equip-interface-monitor.page.tsx
+++ src/components/pages/eqm/equip-interface-monitor.page.tsx
@@ -2,54 +2,6 @@ import React, { useState, useEffect } from "react";
 import { IDefineMonitorProps } from "~/components/UI/card/monitor.ui";
 import { MonitorCards, ITpMonitorCardsProps } from "~/components/UI/card/monitors.ui";
 
-const dummyEquips: IDefineMonitorProps[] = [
-  {
-    title: "설비1",
-    content: "55%",
-    footer: "가동 8h | 목표 12h",
-    status: "Run",
-  },
-  {
-    title: "설비2",
-    content: "",
-    footer: "",
-    status: "Stop",
-  },
-  {
-    title: "설비3",
-    content: "",
-    footer: "",
-    status: "Downtime",
-  },
-];
-
-const searchNewEquips: IDefineMonitorProps[] = [
-  {
-    title: "설비1",
-    content: "55%",
-    footer: "가동 8h | 목표 12h",
-    status: "Run",
-  },
-  {
-    title: "설비2",
-    content: "55%",
-    footer: "가동 8h | 목표 12h",
-    status: "Run",
-  },
-  {
-    title: "설비3",
-    content: "",
-    footer: "",
-    status: "Run",
-  },
-  {
-    title: "설비4",
-    content: "",
-    footer: "",
-    status: "Stop",
-  },
-];
-
 export const PgEqmInterfaceMonitor = () => {
   const [equipStatus, setEquipStatus] = useState<IDefineMonitorProps[]>([]);
 
@@ -58,11 +10,11 @@ export const PgEqmInterfaceMonitor = () => {
   };
 
   const handleSearchButtonClick = () => {
-    setEquipStatus(searchNewEquips);
+    setEquipStatus([]);
   };
 
   useEffect(() => {
-    setEquipStatus(dummyEquips);
+    setEquipStatus([]);
   }, []);
 
   return (
