5d5cda57b5e4c82071d485a91779fde724ba97d9
rjh <jh.ra@kisos.net>
2022-04-28 09:53:38 +0900

    라인차트 컴포넌트 생성

---
+++ src/components/UI/graph/chart-line.ui.tsx
@@ -0,0 +1,35 @@
+import React from "react";
+import {
+  Chart as ChartJS,
+  CategoryScale,
+  LinearScale,
+  PointElement,
+  LineElement,
+  TimeScale,
+  Title,
+  Tooltip,
+  Legend
+} from "chart.js";
+import { Line } from "react-chartjs-2";
+import "chartjs-adapter-moment";
+
+export interface ILineChartProps {
+  options: any;
+  data: {
+    datasets: any;
+  };
+}
+
+ChartJS.register(
+  CategoryScale,
+  LinearScale,
+  TimeScale,
+  PointElement,
+  LineElement,
+  Title,
+  Tooltip,
+  Legend
+);
+const LineChart: React.FC<ILineChartProps> = (props) => (<Line {...props} />);
+
+export default LineChart;
