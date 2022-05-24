9f3ecd926475d4230a8a4d7ac8d7a1aecd46633a
rjh <jh.ra@kisos.net>
2022-05-24 17:06:26 +0900

    서비스 메서드 호출로 변경

--- src/components/pages/prd/progress-history.tsx
+++ src/components/pages/prd/progress-history.tsx
@@ -4,17 +4,17 @@ import { dataGridEvents, getData, getPageName, getToday } from '~/functions';
 import Modal from 'antd/lib/modal/Modal';
 import { TpSingleGrid } from '~/components/templates';
 import ITpSingleGridProps from '~/components/templates/grid-single/grid-single.template.type';
-import { ENUM_WIDTH } from '~/enums';
-import dayjs from 'dayjs';
-import { message } from 'antd';
 import {
   concreteProgressHistoryGridColumns,
   searchFields,
+  rowSpanKeys,
 } from './progress-history/constant';
+import ProgressHistoryService from './progress-history/ProgressHistoryService';
 
 export const PgPrdProgressHistory = () => {
   const title = getPageName();
   const [, modalContext] = Modal.useModal();
+  const progressHistoryService = new ProgressHistoryService();
 
   searchFields[0].defaults = [getToday(-6), getToday()];
 
@@ -22,62 +22,33 @@ export const PgPrdProgressHistory = () => {
 
   const grid = useGrid('GRID', concreteProgressHistoryGridColumns);
 
-  const concatColumns = (columns, list) => {
-    columns.splice(10, 0, ...list);
-
-    return columns;
-  };
-
-  const invlidOverStartDateAtEndDate = () => {
-    message.warning('시작일은 종료일을 넘을 수 없습니다');
-    return false;
-  };
-
-  const invalidOverEndDateAtNow = () => {
-    message.warning('종료일은 현재 날짜를 넘을 수 없습니다');
-    return false;
-  };
-
-  const isValidSearchCondition = ({ start_date, end_date }) => {
-    return start_date > end_date
-      ? invlidOverStartDateAtEndDate()
-      : end_date > dayjs(new Date()).format('YYYY-MM-DD')
-      ? invalidOverEndDateAtNow()
-      : true;
-  };
-
   const onSearch = async searchConditions => {
-    const { raws, value } = isValidSearchCondition(searchConditions)
-      ? await getData(searchConditions, 'prd/multi-proc-by-orders', 'datas')
-      : { raws: [], value: { proc_nos: [] } };
+    if (!progressHistoryService.isValidSearchCondition(searchConditions)) {
+      return console.trace(
+        '%c공정 별 진행 현황 조회 조건 유효성 검사 실패함',
+        'color: red; font-size: 15px;',
+      );
+    }
+
+    const { raws, value } = await getData(
+      searchConditions,
+      'prd/multi-proc-by-orders',
+      'datas',
+    );
 
     for (let index = 0; index < raws.length / 5; index++) {
       raws[index * 5]._attributes = {
-        rowSpan: {
-          reg_date: 5,
-          workings_nm: 5,
-          order_no: 5,
-          order_state: 5,
-          prod_no: 5,
-          prod_nm: 5,
-          item_type_nm: 5,
-          prod_std: 5,
-        },
+        rowSpan: progressHistoryService.spanObject(rowSpanKeys, 5),
       };
     }
 
-    grid.setGridData(raws);
     grid.setGridColumns(
-      concatColumns(
-        columns,
-        value.proc_nos.map(key => ({
-          heaer: key,
-          name: key,
-          width: ENUM_WIDTH.S,
-          filter: 'text',
-        })),
+      progressHistoryService.dynamicColumns(
+        concreteProgressHistoryGridColumns,
+        value.proc_nos.map(progressHistoryService.columnAttributes),
       ),
     );
+    grid.setGridData(raws);
   };
 
   const buttonActions = {
