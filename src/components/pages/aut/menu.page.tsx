17299763dc14763a5f42b54620dc0d84cfabc7e0
rjh <jh.ra@kisos.net>
2022-05-24 13:44:42 +0900

    menu service 인스턴스의 메서드를 호출하는 식으로 변경

--- src/components/pages/aut/menu.page.tsx
+++ src/components/pages/aut/menu.page.tsx
@@ -10,6 +10,7 @@ import {
   menuGridOptions,
   menuInputGroupBoxs,
   menuSearchButtonProps,
+  messages,
 } from './menu/constant/constant';
 import MenuService from './menu/MenuService';
 
@@ -29,64 +30,28 @@ export const PgAutMenu = () => {
   detailModalProps.onCancel = menuService.afterCloseMenuModal;
   detailModalProps.afterClose = () => {
     inputGroups.ref.current.setValues('');
-  }; // Popup unmount 시 input 초기화
+  };
 
-  detailModalProps.onOk = async _ => {
+  detailModalProps.onOk = async () => {
     const record = inputGroups.ref.current.values;
-    let restAPIType = null;
-    let msg = '';
+    const type = menuService.isNewReocrd(record)
+      ? 'post'
+      : menuService.isDeleteReocrd(record)
+      ? 'delete'
+      : 'put';
+    const isValid = menuService.isNewReocrd(record)
+      ? menuService.newRecordValid(record, messages.wargning[type])
+      : menuService.isDeleteReocrd(record)
+      ? menuService.deleteRecordValid(record)
+      : menuService.updateRecordValid(record, messages.wargning[type]);
 
-    const editChk = () => {
-      if (record.uuid == null || record.uuid == '') {
-        // [메뉴 이름] 없으면 신규 메뉴 생성으로 판단
-        restAPIType = 'post'; //생성
-        msg = '생성';
-        if (record.parent_uuid == null || record.parent_uuid == '') {
-          alert('메뉴를 ' + msg + '하시려면 [상위메뉴 이름] 을 입력하세요!');
-          return false;
-        } else if (record.menu_nm == null || record.menu_nm == '') {
-          alert('메뉴를 ' + msg + '하시려면 [신규메뉴 이름] 을 입력하세요!');
-          return false;
-        } else if (record.sortby == null || record.sortby == '') {
-          alert('메뉴를 ' + msg + '하시려면 [정렬순서] 값을 입력하세요!');
-          return false;
-        } else if (record.use_fg == null || record.use_fg == '') {
-          alert(
-            '메뉴를 ' +
-              msg +
-              '하시려면 [사용유무] 값 [True/False] 를 입력하세요!',
-          );
-          return false;
-        }
-      } else if (
-        (record.parent_uuid == null || record.parent_uuid == '') &&
-        (record.sortby == null || record.sortby == '')
-      ) {
-        // [상위메뉴 이름] 과 [정렬순서] 없으면 삭제로 판단
-        restAPIType = 'delete'; //삭제
-        msg = '삭제';
-      } else {
-        // 나머지 경우 모두 수정으로 판단
-        restAPIType = 'put'; //수정
-        msg = '수정';
-        if (record.parent_uuid == null || record.parent_uuid == '') {
-          alert('메뉴를 ' + msg + '하시려면 [상위메뉴 이름] 을 입력하세요!');
-          return false;
-        } else if (record.sortby == null || record.sortby == '') {
-          alert('메뉴를 ' + msg + '하시려면 [정렬순서] 값을 입력하세요!');
-          return false;
-        }
-        record.menu_nm = record.menu_nm_edit;
-      }
-
-      if (confirm('메뉴를 [' + msg + '] 하시겠습니까?') == true) {
-      } else {
-        return false;
-      }
-    };
-    if (editChk() != false) {
-      await executeData([record], URL_PATH_AUT.MENU.PUT.MENUS, restAPIType);
-      alert('메뉴 적용은 재시작이 필요합니다.');
+    if (
+      isValid
+        ? confirm(`메뉴를 ${messages.confirm[type]} 하시겠습니까?`)
+        : false
+    ) {
+      await executeData([record], URL_PATH_AUT.MENU.PUT.MENUS, type);
+      alert(messages.complete);
     }
   };
 
