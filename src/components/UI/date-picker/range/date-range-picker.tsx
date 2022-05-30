c5377aa3af41b846c03b011cc657d6fe920e9008
rjh <jh.ra@kisos.net>
2022-05-30 16:34:10 +0900

    라벨 레인지 피커 호출 코드로 변경

--- src/components/UI/date-picker/range/date-range-picker.tsx
+++ src/components/UI/date-picker/range/date-range-picker.tsx
@@ -1,118 +1,19 @@
-import React, { useCallback } from 'react';
-import dayjs, { Dayjs } from 'dayjs';
-import advancedFormat from 'dayjs/plugin/advancedFormat';
-import customParseFormat from 'dayjs/plugin/customParseFormat';
-import localeData from 'dayjs/plugin/localeData';
-import weekday from 'dayjs/plugin/weekday';
-import weekOfYear from 'dayjs/plugin/weekOfYear';
-import weekYear from 'dayjs/plugin/weekYear';
-import timezone from 'dayjs/plugin/timezone';
-import Colors from '~styles/color.style.scss';
-import Sizes from '~styles/size.style.scss';
-import Fonts from '~styles/font.style.scss';
-import styled from 'styled-components';
-import { DatePicker, Space } from 'antd';
-import { getNow } from '~/functions';
-import { Label } from '../../label';
-import { Checkbox } from '../../checkbox';
+import React from 'react';
+import BaseRangePicker from './base-date-range-picker';
+import LabelRangeDataPiacker, {
+  LabelRangeDatePickerProps,
+} from './label-date-range-picker';
 
-dayjs.locale('ko-kr');
+export interface RangeDatePickerProps extends LabelRangeDatePickerProps {}
 
-dayjs.extend(customParseFormat);
-dayjs.extend(advancedFormat);
-dayjs.extend(weekday);
-dayjs.extend(localeData);
-dayjs.extend(weekOfYear);
-dayjs.extend(weekYear);
-dayjs.extend(timezone);
-
-export interface RangeDatePickerProps {
-  id: string;
-  ids: string;
-  names?: string;
-  placeholder?: string;
-  checkbox: boolean;
-  checked: boolean;
-  checkBoxOnChange: Function;
-  label?: string;
-  defaultValue?: string | any;
-  important?: boolean;
-  disabled?: boolean;
-  returnType?: 'date' | 'dateString';
-  widthSize?: 'default' | 'auto' | 'flex' | number | string;
-  onChange?: (date: Dayjs, dateString?: string) => void;
-}
-
-export const ReangePickerWrapper = styled(DatePicker.RangePicker)`
-  width: ${props =>
-    props.widthSize === 'flex'
-      ? '100%'
-      : props.widthSize === 'auto'
-      ? 'auto'
-      : props.widthSize
-      ? `${props.widthSize}px`
-      : Sizes.width_datepicker_md};
-
-  height: ${Sizes.height_datepicker_default};
-  border-radius: ${Sizes.borderRadius_common};
-  border-color: ${Colors.bg_datepicker_border};
-  /* margin-bottom: 8px; */
-
-  &:hover,
-  &:focus {
-    border-color: ${Colors.bg_datepicker_border};
+const RangePicker: React.FC<RangeDatePickerProps> = ({
+  checkbox,
+  label,
+  ...rangepicker
+}) => {
+  if (label) {
+    return <LabelRangeDataPiacker {...{ label, ...rangepicker }} />;
   }
-  ::selection {
-    color: ${Colors.fg_datepicker_selection};
-    background: ${Colors.bg_datepicker_selection};
-  }
-  .ant-picker-input > input {
-    font-size: ${Fonts.fontSize_datepicker};
-    letter-spacing: ${Sizes.letterSpacing_common};
-  }
-`;
-
-const RangePicker: React.FC<RangeDatePickerProps> = props => {
-  const onChange = useCallback(
-    (dates, dateStrings) => {
-      let returnValue = props.returnType === 'dateString' ? dateStrings : dates;
-
-      if ((returnValue === '' || returnValue == null) && props.defaultValue)
-        returnValue = props.defaultValue;
-
-      if (props.onChange) props.onChange(returnValue);
-    },
-    [props.onChange],
-  );
-
-  return (
-    <>
-      <Space size={10} wrap>
-        {props.checkbox ? (
-          <Checkbox
-            id={props.id + '_chk'}
-            code={props.id + '_chk'}
-            name={(props.name || props.id) + '_chk'}
-            text={props.label}
-            checked={props.checked}
-            onChange={props.checkBoxOnChange}
-          />
-        ) : (
-          <Label text={props.label} important={props.important} />
-        )}
-        <ReangePickerWrapper
-          ids={props.ids}
-          names={props.names}
-          defaultValue={props.defalutValue}
-          onChange={onChange}
-          disabledDate={current => current > dayjs(getNow(0).substring(0, 10))}
-          placeholder={props.placeholder}
-          disabled={props.disabled}
-          widthSize={props.widthSize}
-        />
-      </Space>
-    </>
-  );
 };
 
 export default RangePicker;
