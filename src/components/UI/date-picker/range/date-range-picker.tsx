243ac686e0b19bbe736b694001eddd3abfdde91a
rjh <jh.ra@kisos.net>
2022-05-30 16:58:09 +0900

    조건에 따라 [checkbox, label, base] rangepicker를 호출

--- src/components/UI/date-picker/range/date-range-picker.tsx
+++ src/components/UI/date-picker/range/date-range-picker.tsx
@@ -1,19 +1,27 @@
 import React from 'react';
-import BaseRangePicker from './base-date-range-picker';
-import LabelRangeDataPiacker, {
-  LabelRangeDatePickerProps,
-} from './label-date-range-picker';
+import BaseRangePicker, {
+  BaseRangeDatePickerProps,
+} from './base-date-range-picker';
+import CheckBoxRangeDataPiacker from './checkbox-date-range-picker';
+import LabelRangeDataPiacker from './label-date-range-picker';
 
-export interface RangeDatePickerProps extends LabelRangeDatePickerProps {}
+export interface RangeDatePickerProps extends BaseRangeDatePickerProps {
+  ILabelProps;
+  ICheckboxProps;
+}
 
 const RangePicker: React.FC<RangeDatePickerProps> = ({
   checkbox,
   label,
-  ...rangepicker
+  ...pickerProps
 }) => {
-  if (label) {
-    return <LabelRangeDataPiacker {...{ label, ...rangepicker }} />;
-  }
+  return checkbox ? (
+    <CheckBoxRangeDataPiacker {...{ checkbox, ...pickerProps }} />
+  ) : label ? (
+    <LabelRangeDataPiacker {...{ label, ...pickerProps }} />
+  ) : (
+    <BaseRangePicker {...pickerProps} />
+  );
 };
 
 export default RangePicker;
