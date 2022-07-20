7b756bc8aa3e1a7f8589a7e14c1a717e46f9b087
rjh <jh.ra@kisos.net>
2022-07-20 14:45:31 +0900

    로그인 이력 페이지 추가
    브라우저에서 접속 할 수 있는 로그인 이력 페이지를 추가함

--- src/components/pages/index.ts
+++ src/components/pages/index.ts
@@ -69,6 +69,7 @@ export * from './std/customer-price.page';
 export * from './std/routing.page';
 export * from './std/tenant-opt.page';
 export * from './std/excel-upload.page';
+export * from './std/login-history.page';
 
 export * from './prd/return.page';
 export * from './prd/return-report.page';
---
+++ src/components/pages/std/login-history.page.tsx
@@ -0,0 +1,11 @@
+import React from 'react';
+import { Datagrid, Searchbox } from '~/components/UI';
+
+export const PgStdLoginHistory = () => {
+  return (
+    <>
+      <Searchbox />
+      <Datagrid columns={[]} />
+    </>
+  );
+};
