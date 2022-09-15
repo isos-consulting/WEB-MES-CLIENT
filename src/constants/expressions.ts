7d973a072c08594300113e9f4578dc07d2e94834
rjh <jh.ra@kisos.net>
2022-09-15 09:04:27 +0900

    DECIMAL_OPTIONAL_DOT 정규식 수정
    "숫자.숫자" 조합을 체크하는 정규식이 틀려서 수정함

--- src/constants/expressions.ts
+++ src/constants/expressions.ts
@@ -2,11 +2,10 @@ const EXPRESSSIONS = {
   HOUR_MINUTE: /^([1-9]|[01]\d|2[0-3]):([0-5]\d)$/,
   HOUR_MINUTE_SECOND: /^([1-9]|[01]\d|2[0-3]):([0-5]\d):([0-5]\d)$/,
   NON_DIGIT_GLOBAL: /\D/g,
-  DECIMAL_OPTIONAL_SIGN_COMMA_DOT_GLOBAL:
+  DECIMAL_OPTIONAL_SIGN_COMMA_DOT:
     /^[+\-]?(([1-9]\d{0,2}(,\d{3})*)|\d+){1}(\.\d+)?$/,
-  DECIMAL_OPTIONAL_COMMA_DOT_GLOBAL:
-    /^(([1-9]\d{0,2}(,\d{3})*)|\d+){1}(\.\d+)?$/,
-  DECIMAL_OPTIONAL_DOT_GLOBAL: /\D+(\.\d+)?$/,
+  DECIMAL_OPTIONAL_COMMA_DOT: /^(([1-9]\d{0,2}(,\d{3})*)|\d+){1}(\.\d+)?$/,
+  DECIMAL_OPTIONAL_DOT: /^\d+(\.\d+)?$/,
 };
 
 export default EXPRESSSIONS;
