6c68843864790119fbbb968b71573034085f4cdd
rjh <jh.ra@kisos.net>
2022-09-15 09:02:51 +0900

    jest 설정 파일 확장자를 js에서 ts로 변경
    파일 변경할 때마다 lint error 발생하는게 불편하여 변경함

---
+++ config/jest.config.ts
@@ -0,0 +1,25 @@
+import type { Config } from 'jest';
+
+const config: Config = {
+  preset: 'ts-jest',
+  rootDir: '../',
+  roots: ['<rootDir>/src', '<rootDir>/tests'],
+  verbose: true,
+  testEnvironment: 'node',
+  transform: {
+    '^.+\\.ts?$': 'ts-jest',
+  },
+  transformIgnorePatterns: [
+    '<rootDir>/node_modules/',
+    '<rootDir>/dist/',
+    '<rootDir>/build/',
+    '<rootDir>/coverage/',
+    '<rootDir>/docs/',
+  ],
+  moduleFileExtensions: ['ts', 'js', 'tsx', 'jsx'],
+  moduleNameMapper: {
+    '^~/(.*)$': '<rootDir>/src/$1',
+  },
+};
+
+export default config;
