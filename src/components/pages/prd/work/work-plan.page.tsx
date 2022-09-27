f937f0c04ab52ffe3c83420108c2084a62e7c40f
rjh <jh.ra@kisos.net>
2022-09-27 14:44:06 +0900

    생산계획 추가, 수정 모달 추가
    생산 계획 정보를 추가하거나 수정할 수 있는 UI 추가함

--- src/components/pages/prd/work/work-plan.page.tsx
+++ src/components/pages/prd/work/work-plan.page.tsx
@@ -1,4 +1,5 @@
-import React from 'react';
+import { Modal } from 'antd';
+import React, { useState } from 'react';
 import {
   Button,
   Datagrid,
@@ -7,8 +8,22 @@ import {
   useSearchbox,
 } from '~/components/UI';
 import { ColumnStore } from '~/constants/columns';
+import { getPageName } from '~/functions';
+import BasicModalContext from '../../adm/excel-upload-type/hooks/modal';
 
+const hiddenWokrPlanModal = new BasicModalContext({
+  title: '',
+  columns: ColumnStore.WORK_PLAN,
+  visible: false,
+  gridMode: 'view',
+  data: [],
+  gridPopupInfo: [],
+  gridComboInfo: [],
+  onOk: () => {},
+});
 export const PgWorkPlan = () => {
+  const title = getPageName();
+  const [workPlanData, setWorkPlanData] = useState([]);
   const workPlanSearchInfo = useSearchbox(
     'workPlanSearchInfo',
     [{ type: 'date', id: 'plan_date', default: '2022-09', label: '계획월' }],
@@ -17,7 +32,55 @@ export const PgWorkPlan = () => {
       console.log('데이터 그리드에 검색조건을 적용하여 조회를 수행합니다');
     },
   );
-  const workPlanDataGridRef = React.createRef();
+
+  const [workPlanModalContext, modalContextSwitch] =
+    useState(hiddenWokrPlanModal);
+
+  const hideWokrPlanModal = () => modalContextSwitch(hiddenWokrPlanModal);
+
+  const getWorkPlanData = () => [];
+
+  const confirmAtBeforeCallApi = () =>
+    Modal.confirm({
+      icon: null,
+      title: '저장하시겠습니까?',
+      content: '생산 계획 정보를 저장하시겠습니까?',
+      onOk: modalApiCallSuccess,
+    });
+
+  const modalApiCallSuccess = () => {
+    Promise.resolve({ success: true }).then(res => {
+      if (res.success === true) {
+        modalContextSwitch(hiddenWokrPlanModal);
+        setWorkPlanData(getWorkPlanData);
+      }
+    });
+  };
+
+  const showAddWorkPlanModal = () =>
+    modalContextSwitch({
+      ...BasicModalContext.add({
+        title,
+        columns: ColumnStore.WORK_PLAN,
+        gridPopupInfo: [],
+        gridComboInfo: [],
+        onOk: confirmAtBeforeCallApi,
+      }),
+      disabledAutoDateColumn: true,
+    });
+
+  const showEditWorkPlanModal = () =>
+    modalContextSwitch(
+      BasicModalContext.edit({
+        title,
+        columns: ColumnStore.WORK_PLAN,
+        data: [...workPlanData],
+        gridPopupInfo: [],
+        gridComboInfo: [],
+        onOk: confirmAtBeforeCallApi,
+      }),
+    );
+
   return (
     <>
       <header style={{ width: '100%' }}>
@@ -31,14 +94,14 @@ export const PgWorkPlan = () => {
           </Button>
           <Button
             onClick={() => {
-              console.log('수정 모달 창 나옴');
+              showEditWorkPlanModal();
             }}
           >
             수정
           </Button>
           <Button
             onClick={() => {
-              console.log('신규항목추가 모달 창 나옴');
+              showAddWorkPlanModal();
             }}
           >
             신규항목추가
@@ -51,13 +114,9 @@ export const PgWorkPlan = () => {
           <Datagrid data={[]} columns={ColumnStore.WORK_PLAN} />
         </div>
       </main>
-      <GridPopup
-        gridref={workPlanDataGridRef}
-        columns={ColumnStore.WORK_PLAN}
-        visible={false}
-        gridMode="create"
-        disabledAutoDateColumn={true}
-      />
+      {workPlanModalContext.visible === true && (
+        <GridPopup {...workPlanModalContext} onCancel={hideWokrPlanModal} />
+      )}
     </>
   );
 };
