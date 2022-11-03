ca5ec3b47c00ccc248db3ab106303d36a8f80be7
rjh <jh.ra@kisos.net>
2022-11-03 17:01:38 +0900

    다중 선택 콤보박스의 라벨의 값이 입력되었을 경우에만 표현되도록 변경

--- src/components/UI/combobox/multi-select/multi-selectable.combobox.ui.tsx
+++ src/components/UI/combobox/multi-select/multi-selectable.combobox.ui.tsx
@@ -4,25 +4,26 @@ import { Label } from '../../label';
 import { ScCombobox } from '../combobox.ui.styled';
 import { useComboDatas } from './data-setting-option.hook';
 
+const MultiSelectableComboboxLabel = ({ label, important }) => {
+  if (label) return <Label text={label} {...important} />;
+
+  return null;
+};
+
 export const MultiSelectableCombobox = props => {
+  const { dataSettingOptions, label, important, ...comboboxProps } = props;
   const { comboData } = useComboDatas({
     ...props.dataSettingOptions,
   });
 
   return (
     <Space size={10} wrap>
-      <Label text={props.label} important={props.important}></Label>
+      <MultiSelectableComboboxLabel {...{ label, important }} />
       <ScCombobox
         mode="multiple"
         maxTagCount="responsive"
         allowClear={true}
-        defaultValue={props.defaultValue}
-        value={props.value}
-        onChange={props.onChange}
-        disabled={props.disabled}
-        widthSize={props.widthSize}
-        fontSize={props.fontSize}
-        placeholder={props.placeholder}
+        {...comboboxProps}
         options={comboData}
       />
     </Space>
