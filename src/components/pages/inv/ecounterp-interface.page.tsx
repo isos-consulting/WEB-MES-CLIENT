1eb02b67e76d138c259039ed8a94b8c6623d2007
rjh <jh.ra@kisos.net>
2022-11-08 10:52:13 +0900

    이카운트 ERP 생산불출 데이터 연동 기능 추가

--- src/components/pages/inv/ecounterp-interface.page.tsx
+++ src/components/pages/inv/ecounterp-interface.page.tsx
@@ -10,8 +10,73 @@ import { ButtonStore } from '~/constants/buttons';
 import { ColumnStore } from '~/constants/columns';
 import Excel from 'exceljs';
 
+const columns = {
+  구매: ColumnStore.INCOME_STORE_ECOUNT_INTERFACE,
+  생산불출: ColumnStore.OUT_STORE_ECOUNT_INTERFACE,
+};
+
+const extractModalContext = name => {
+  return {
+    title: `${name} 등록`,
+    columns: columns[name],
+    extraButtons: [
+      {
+        align: 'right',
+        buttonProps: {
+          text: '파일 선택',
+          imgtype: 'add',
+        },
+        buttonAction: (_event, _buttonProps, gridProps) => {
+          const file = document.createElement('input');
+          file.type = 'file';
+          file.click();
+          file.addEventListener('change', async e => {
+            const readFile = (file): Promise<ArrayBuffer> => {
+              return new Promise((resolve, reject) => {
+                const reader = new FileReader();
+                reader.onload = () => resolve(reader.result as ArrayBuffer);
+                reader.onerror = reject;
+                reader.readAsArrayBuffer(file);
+              });
+            };
+            const file = (e.target as HTMLInputElement).files[0];
+            const excelFileAsBuffer = await readFile(file);
+
+            const workBook = new Excel.Workbook();
+            const uploadData = await workBook.xlsx.load(excelFileAsBuffer);
+            const sheets = uploadData.worksheets;
+            const incomeSheet = sheets.filter(sheet =>
+              sheet.name.includes(name),
+            );
+
+            if (incomeSheet.length > 0) {
+              const incomeData = incomeSheet[0].getSheetValues();
+              const incomeDataWithoutHeader = incomeData.filter(
+                row => row.length > columns[name].length,
+              );
+
+              const filterdData = incomeDataWithoutHeader.slice(1).map(row => {
+                const obj = {};
+                for (let i = 1; i < row.length; i++) {
+                  obj[incomeDataWithoutHeader[0][i]] = row[i];
+                }
+
+                return obj;
+              });
+              gridProps.gridRef.current.getInstance().appendRows(filterdData);
+            }
+          });
+        },
+      },
+    ],
+  };
+};
+
 export const PgInvEcountERPInterface = () => {
   const [visible, setVisible] = useState(false);
+  const [modalContext, setModalContext] = useState({
+    ...extractModalContext('구매'),
+  });
   return (
     <>
       <Container>
@@ -27,6 +92,7 @@ export const PgInvEcountERPInterface = () => {
               )}`,
               onClick: () => {
                 setVisible(true);
+                setModalContext(extractModalContext('구매'));
               },
             },
             {
@@ -36,7 +102,10 @@ export const PgInvEcountERPInterface = () => {
                 '엑셀',
                 '',
               )}`,
-              onClick: () => {},
+              onClick: () => {
+                setVisible(true);
+                setModalContext(extractModalContext('생산불출'));
+              },
             },
           ]}
         />
@@ -49,68 +118,13 @@ export const PgInvEcountERPInterface = () => {
       </Container>
       <GridPopup
         visible={visible}
-        title="입고 등록"
-        columns={ColumnStore.INCOME_STORE_ECOUNT_INTERFACE}
         onOk={() => {
           setVisible(false);
         }}
         onCancel={() => {
           setVisible(false);
         }}
-        extraButtons={[
-          {
-            align: 'right',
-            buttonProps: {
-              text: '엑셀 파일 선택',
-              imgtype: 'add',
-            },
-            buttonAction: (_event, buttonProps, gridProps) => {
-              const file = document.createElement('input');
-              file.type = 'file';
-              file.click();
-              file.addEventListener('change', async e => {
-                const readFile = (file): Promise<ArrayBuffer> => {
-                  return new Promise((resolve, reject) => {
-                    const reader = new FileReader();
-                    reader.onload = () => resolve(reader.result as ArrayBuffer);
-                    reader.onerror = reject;
-                    reader.readAsArrayBuffer(file);
-                  });
-                };
-                const file = (e.target as HTMLInputElement).files[0];
-                const excelFileAsBuffer = await readFile(file);
-
-                const workBook = new Excel.Workbook();
-                const uploadData = await workBook.xlsx.load(excelFileAsBuffer);
-                const sheets = uploadData.worksheets;
-                const incomeSheet = sheets.filter(sheet =>
-                  sheet.name.includes('구매'),
-                );
-
-                if (incomeSheet.length > 0) {
-                  const incomeData = incomeSheet[0].getSheetValues();
-                  const incomeDataWithoutHeader = incomeData.filter(
-                    row => row.length === 31,
-                  );
-
-                  const filterdData = incomeDataWithoutHeader
-                    .slice(1)
-                    .map(row => {
-                      const obj = {};
-                      for (let i = 1; i < row.length; i++) {
-                        obj[incomeDataWithoutHeader[0][i]] = row[i];
-                      }
-
-                      return obj;
-                    });
-                  gridProps.gridRef.current
-                    .getInstance()
-                    .appendRows(filterdData);
-                }
-              });
-            },
-          },
-        ]}
+        {...modalContext}
         disabledAutoDateColumn={true}
       ></GridPopup>
     </>
