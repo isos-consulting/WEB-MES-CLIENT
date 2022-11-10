090a274c46308779abb66c355b4e0fd097216eee
rjh <jh.ra@kisos.net>
2022-11-10 15:54:37 +0900

    구매, 생산불출 시트를 데이터 그리드에 표현하는 함수 분리

--- src/components/pages/inv/ecounterp-interface.page.tsx
+++ src/components/pages/inv/ecounterp-interface.page.tsx
@@ -17,6 +17,42 @@ const columns = {
   생산불출: ColumnStore.OUT_STORE_ECOUNT_INTERFACE,
 };
 
+const readExcelFile = (file: File): Promise<ArrayBuffer> => {
+  return new Promise((resolve, reject) => {
+    const reader = new FileReader();
+    reader.onload = () => resolve(reader.result as ArrayBuffer);
+    reader.onerror = reject;
+    reader.readAsArrayBuffer(file);
+  });
+};
+
+const importExcelFile = (excelFile: File, name: string) => {
+  return async gridProps => {
+    const excelFileAsBuffer = await readExcelFile(excelFile);
+    const sheets = await (
+      await new Excel.Workbook().xlsx.load(excelFileAsBuffer)
+    ).worksheets;
+    const selectedSheet = sheets.filter(sheet => sheet.name.includes(name));
+
+    if (selectedSheet.length > 0) {
+      const data = selectedSheet[0].getSheetValues();
+      const dataWithoutHeader = data.filter(
+        row => row.length > columns[name].length,
+      );
+
+      const filterdData = dataWithoutHeader.slice(1).map(row => {
+        const obj = {};
+        for (let i = 1; i < row.length; i++) {
+          obj[dataWithoutHeader[0][i]] = row[i];
+        }
+
+        return obj;
+      });
+      gridProps.gridRef.current.getInstance().appendRows(filterdData);
+    }
+  };
+};
+
 const extractModalContext = name => {
   return {
     title: `${name} 등록`,
@@ -32,41 +68,12 @@ const extractModalContext = name => {
           const file = document.createElement('input');
           file.type = 'file';
           file.click();
+
           file.addEventListener('change', async e => {
-            const readFile = (file): Promise<ArrayBuffer> => {
-              return new Promise((resolve, reject) => {
-                const reader = new FileReader();
-                reader.onload = () => resolve(reader.result as ArrayBuffer);
-                reader.onerror = reject;
-                reader.readAsArrayBuffer(file);
-              });
-            };
-            const file = (e.target as HTMLInputElement).files[0];
-            const excelFileAsBuffer = await readFile(file);
-
-            const workBook = new Excel.Workbook();
-            const uploadData = await workBook.xlsx.load(excelFileAsBuffer);
-            const sheets = uploadData.worksheets;
-            const incomeSheet = sheets.filter(sheet =>
-              sheet.name.includes(name),
-            );
-
-            if (incomeSheet.length > 0) {
-              const incomeData = incomeSheet[0].getSheetValues();
-              const incomeDataWithoutHeader = incomeData.filter(
-                row => row.length > columns[name].length,
-              );
-
-              const filterdData = incomeDataWithoutHeader.slice(1).map(row => {
-                const obj = {};
-                for (let i = 1; i < row.length; i++) {
-                  obj[incomeDataWithoutHeader[0][i]] = row[i];
-                }
-
-                return obj;
-              });
-              gridProps.gridRef.current.getInstance().appendRows(filterdData);
-            }
+            await importExcelFile(
+              (e.target as HTMLInputElement).files[0],
+              name,
+            )(gridProps);
           });
         },
       },
@@ -88,7 +95,7 @@ export const PgInvEcountERPInterface = () => {
       },
     ],
     () => {
-      console.log('hello');
+      // 조회 버튼을 클릭했을 때 실행되는 함수를 작성하시오
     },
   );
   const [modalContext, setModalContext] = useState({
