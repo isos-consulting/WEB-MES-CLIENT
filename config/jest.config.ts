2c6f6b98881a297aea7718fb19b0ddb3ba988300
rjh <jh.ra@kisos.net>
2022-12-15 14:19:13 +0900

    jest 설정 변경

--- config/jest.config.ts
+++ config/jest.config.ts
@@ -7,7 +7,7 @@ const config: Config = {
   verbose: true,
   testEnvironment: 'node',
   transform: {
-    '^.+\\.ts?$': 'ts-jest',
+    '^.+\\.ts?$': '@swc/jest',
   },
   transformIgnorePatterns: [
     '<rootDir>/node_modules/',
