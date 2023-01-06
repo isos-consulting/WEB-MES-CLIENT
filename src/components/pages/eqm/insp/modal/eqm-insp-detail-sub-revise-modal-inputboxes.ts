4cdcca0d290e939d082b38ec9edaf7962b3faf87
rjh <jh.ra@kisos.net>
2023-01-06 11:07:15 +0900

    설비 점검 기준서 개정 입력 폼 정보 추가

---
+++ src/components/pages/eqm/insp/modal/eqm-insp-detail-sub-revise-modal-inputboxes.ts
@@ -0,0 +1,51 @@
+import { getPopupForm } from '~/components/UI';
+import { IInputGroupboxItem } from '~/components/UI/input-groupbox';
+import { getToday } from '~/functions';
+
+export default [
+  {
+    type: 'text',
+    id: 'insp_uuid',
+    alias: 'uuid',
+    label: '기준서UUID',
+    disabled: true,
+    hidden: true,
+  },
+  {
+    type: 'text',
+    id: 'equip_uuid',
+    label: '설비UUID',
+    disabled: true,
+    hidden: true,
+  },
+  {
+    type: 'text',
+    id: 'equip_cd',
+    label: '설비코드',
+    disabled: true,
+    usePopup: true,
+    popupKeys: ['equip_uuid', 'equip_cd', 'equip_nm'],
+    popupButtonSettings: {
+      dataApiSettings: {
+        uriPath: getPopupForm('설비관리').uriPath,
+        params: { use_fg: true },
+      },
+      datagridSettings: getPopupForm('설비관리').datagridProps,
+      modalSettings: {
+        title: '품목관리',
+      },
+    },
+  },
+  { type: 'text', id: 'equip_nm', label: '설비', disabled: true },
+  { type: 'text', id: 'insp_no', label: '기준서 번호', disabled: true },
+  {
+    type: 'date',
+    id: 'reg_date',
+    label: '생성일자',
+    disabled: false,
+    default: getToday(0, { format: 'YYYY-MM-DD' }),
+    required: true,
+  },
+  { type: 'text', id: 'contents', label: '개정내역', disabled: false },
+  { type: 'text', id: 'remark', label: '비고', disabled: false },
+] as IInputGroupboxItem[];
