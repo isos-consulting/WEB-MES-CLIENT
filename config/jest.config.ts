888b8f1650a420ce2e37aa0e78e2d19541fdb64b
rjh <jh.ra@kisos.net>
2023-01-06 14:30:31 +0900

    자격증명 관련된 하드코딩을 환경 변수 파일에서 불러오도록 변경

--- config/jest.config.ts
+++ config/jest.config.ts
@@ -20,6 +20,7 @@ const config: Config = {
   moduleNameMapper: {
     '^~/(.*)$': '<rootDir>/src/$1',
   },
+  setupFiles: ['dotenv/config'],
 };
 
 export default config;
--- tests/aut/user.test.ts
+++ tests/aut/user.test.ts
@@ -1,32 +1,34 @@
 import UserManager, { CurrentUser } from '../../src/models/user/user';
+import dotenv from 'dotenv';
 
+dotenv.config({ path: './.env.test' });
 test('비밀번호 초기화 기능을 사용할 경우 사용자의 비밀번호가 변경 됨', () => {
-  // given
-  const kimOneToFour = new CurrentUser({ id: 'kim', password: '1234' });
+  const kimOneToFour = new CurrentUser({
+    id: 'kim',
+    password: process.env.TEST_USER_PASSWORD,
+  });
   const newPassword = '5678';
 
-  // when
   const kimFiveToEight = kimOneToFour.resetPassword(newPassword);
 
-  // then
   expect(kimOneToFour.password).toBe('1234');
   expect(kimFiveToEight.password).toBe('5678');
 });
 
 test('kim의 비밀번호를 변경해도 장표에 저장 된 kim의 비밀번호는 변경되지 않는다', () => {
-  // given
   const userManager = new UserManager();
   const users = userManager.users();
-  const kim = new CurrentUser({ id: 'kim', password: '1234' });
+  const kim = new CurrentUser({
+    id: 'kim',
+    password: process.env.TEST_USER_PASSWORD,
+  });
 
-  // when
   userManager.add(kim);
   const includesUsers = userManager.users();
 
   const kimFive = kim.resetPassword('5678');
   const includesUsersAfterReset = userManager.users();
 
-  // then
   expect(users).toEqual([]);
   expect(includesUsers).toEqual([kim]);
   expect(includesUsersAfterReset).not.toEqual([kimFive]);
