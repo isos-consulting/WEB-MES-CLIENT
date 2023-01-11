f975802786c2ae21f69d20c35aeed2984334d760
rjh <jh.ra@kisos.net>
2023-01-11 13:39:56 +0900

    주차별 당월 일자를 반환하는 기능 추가

--- src/functions/date.function.ts
+++ src/functions/date.function.ts
@@ -1,17 +1,111 @@
 import moment from 'moment';
 
+enum DateType {
+  'year' = 'year',
+  'month' = 'month',
+  'week' = 'week',
+  'day' = 'day',
+  'hour' = 'hour',
+  'minute' = 'minute',
+  'second' = 'second',
+  'millisecond' = 'millisecond',
+}
+
+class DateArgumentsNullPointException extends Error {
+  constructor(arg: DateType) {
+    super(`${arg} is null or undefined`);
+  }
+}
+
+class MonthInvalidException extends Error {
+  constructor() {
+    super('month is invalid date');
+  }
+}
+
 export const getWeeksAtMonth = month => {
-  if (month == null) throw new Error('month is null or undefined');
+  if (month == null) throw new DateArgumentsNullPointException(DateType.month);
   const momentConvertedMonth = moment(month);
 
   if (momentConvertedMonth.isValid() === false)
-    throw new Error('month is invalid date');
+    throw new MonthInvalidException();
 
   const firstWeek = moment(momentConvertedMonth).startOf('month').week();
   const lastWeek = moment(momentConvertedMonth).endOf('month').week();
+
+  if (lastWeek === 1)
+    return Array.from({ length: 53 - firstWeek + 1 }, (_v, i) => i + firstWeek);
 
   return Array.from(
     { length: lastWeek - firstWeek + 1 },
     (_v, i) => i + firstWeek,
   );
+};
+
+export const getRangeDateAtMonthForWeek = (year, month, week) => {
+  if (year == null) throw new DateArgumentsNullPointException(DateType.year);
+  if (month == null) throw new DateArgumentsNullPointException(DateType.month);
+  if (week == null) throw new DateArgumentsNullPointException(DateType.week);
+  if (week === 53) return getRangeDateAtFiftyThreeWeeks(year);
+
+  const momentConvertedWeek = moment().locale('ko').year(year).week(week);
+
+  const startOfDateForWeek = momentConvertedWeek
+    .startOf('week')
+    .format('YYYY-MM-DD');
+  const endOfDateForWeek = momentConvertedWeek
+    .endOf('week')
+    .format('YYYY-MM-DD');
+
+  const momentDurationAsDays = moment
+    .duration(moment(endOfDateForWeek).diff(moment(startOfDateForWeek)))
+    .asDays();
+
+  const dates = Array.from({ length: momentDurationAsDays + 1 }, (_v, i) =>
+    moment(startOfDateForWeek).add(i, 'days'),
+  );
+
+  const momentConvertedMonth = moment()
+    .locale('ko')
+    .year(year)
+    .month(month - 1);
+  const startOfDateForMonth = momentConvertedMonth
+    .startOf('month')
+    .format('YYYY-MM-DD');
+  const endOfDateForMonth = momentConvertedMonth
+    .endOf('month')
+    .format('YYYY-MM-DD');
+
+  const filterdDates = dates.filter(
+    date =>
+      moment(date).isSameOrAfter(moment(startOfDateForMonth)) &&
+      moment(date).isSameOrBefore(moment(endOfDateForMonth)),
+  );
+
+  return filterdDates.map(date => date.date());
+};
+
+export const getRangeDateAtFiftyThreeWeeks = year => {
+  if (year == null) throw new DateArgumentsNullPointException(DateType.year);
+
+  const momentConvertedYear = moment().locale('ko').year(year);
+  const momentConvertedLastWeek = moment().locale('ko').year(year).week(52);
+
+  const endOfDateForYear = momentConvertedYear
+    .endOf('year')
+    .format('YYYY-MM-DD');
+
+  const endOfDateForLastWeek = momentConvertedLastWeek
+    .endOf('week')
+    .format('YYYY-MM-DD');
+
+  const momentDurationAsDays = moment
+    .duration(moment(endOfDateForYear).diff(moment(endOfDateForLastWeek)))
+    .asDays();
+
+  const dates = Array.from({ length: momentDurationAsDays }, (_v, i) =>
+    moment(endOfDateForLastWeek).add(i + 1, 'days'),
+  );
+
+  return dates.map(date => date.date());
 };
--- tests/util/weeks.test.ts
+++ tests/util/weeks.test.ts
@@ -1,7 +1,65 @@
-import { getWeeksAtMonth } from '~/functions/date.function';
+import {
+  getRangeDateAtFiftyThreeWeeks,
+  getRangeDateAtMonthForWeek,
+  getWeeksAtMonth,
+} from '~/functions/date.function';
 
 test('2023-01은 1~5를 포함한 배열을 반환한다', () => {
   const weeks = getWeeksAtMonth('2023-01');
 
   expect(weeks).toEqual([1, 2, 3, 4, 5]);
+});
+
+test('2023-12은 48~53를 포함한 배열을 반환한다', () => {
+  const weeks = getWeeksAtMonth('2023-12');
+
+  expect(weeks).toEqual([48, 49, 50, 51, 52, 53]);
+});
+
+test('getWeeksMonth 함수는 month 인자가 없으면 에러를 발생시킨다', () => {
+  expect(() => getWeeksAtMonth(null)).toThrowError(
+    'month is null or undefined',
+  );
+});
+
+test('getWeeksMonth 함수는 month 인자가 유효하지 않으면 에러를 발생시킨다', () => {
+  expect(() => getWeeksAtMonth('2023-13')).toThrowError(
+    'month is invalid date',
+  );
+});
+
+test('2023년 1월 5주차는 1월 29~31일을 포함한 배열을 반환한다', () => {
+  const dates = getRangeDateAtMonthForWeek(2023, 1, 5);
+
+  expect(dates).toEqual([29, 30, 31]);
+});
+
+test('2023년 5주차는 2월의 1~4일을 포함한 배열을 반환한다', () => {
+  const dates = getRangeDateAtMonthForWeek(2023, 2, 5);
+
+  expect(dates).toEqual([1, 2, 3, 4]);
+});
+
+test('2023년 53주차는 12월의 31일을 포함한 배열을 반환한다', () => {
+  const dates = getRangeDateAtFiftyThreeWeeks(2023);
+
+  expect(dates).toEqual([31]);
+});
+
+test('getRangeDateAtMonthForWeek 함수는 year 인자가 없으면 에러를 발생시킨다', () => {
+  expect(() => getRangeDateAtMonthForWeek(null, 1, 1)).toThrowError(
+    'year is null or undefined',
+  );
+});
+
+test('getRangeDateAtMonthForWeek 함수는 month 인자가 없으면 에러를 발생시킨다', () => {
+  expect(() => getRangeDateAtMonthForWeek(2023, null, 1)).toThrowError(
+    'month is null or undefined',
+  );
+});
+
+test('getRangeDateAtMonthForWeek 함수는 week 인자가 없으면 에러를 발생시킨다', () => {
+  expect(() => getRangeDateAtMonthForWeek(2023, 1, null)).toThrowError(
+    'week is null or undefined',
+  );
 });
