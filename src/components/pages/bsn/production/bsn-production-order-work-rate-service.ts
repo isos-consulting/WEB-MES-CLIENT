c01a602ce930ecdd7dd4ce8eb9aea6ae4d815cfd
rjh <jh.ra@kisos.net>
2023-01-12 12:20:10 +0900

    생산 금액 달성율 월별, 일별 데이터를 계산하는 기능 추가

--- src/components/pages/bsn/production/bsn-production-order-work-rate-service.ts
+++ src/components/pages/bsn/production/bsn-production-order-work-rate-service.ts
@@ -1,6 +1,8 @@
 import { IGridColumn } from '~/components/UI';
 import { ENUM_DECIMAL, ENUM_WIDTH } from '~/enums';
+import { isNumber } from '~/functions';
 import {
+  getRangeDateAtMonth,
   getRangeDateAtMonthForWeek,
   getWeeksAtMonth,
 } from '~/functions/date.function';
@@ -20,6 +22,73 @@ export class BsnProductionOrderWorkRateService {
     return Number(this.month.substring(5, 7));
   }
 
+  private calcTotal({ fg, ...prices }) {
+    return Object.values(prices).reduce((acc, cur) => {
+      if (isNumber(cur)) return acc + Number(cur);
+
+      return acc;
+    }, 0);
+  }
+
+  monthColumn(): IGridColumn[] {
+    const year = this.getCastedYearForNumber();
+    const monthColumns = Array(12)
+      .fill(0)
+      .map((_, i): IGridColumn => {
+        const month = i + 1;
+        const monthKey = month > 9 ? `${month}` : `0${month}`;
+
+        return {
+          header: `${month}월`,
+          name: `${year}-${monthKey}`,
+          format: 'number',
+          decimal: ENUM_DECIMAL.DEC_PRICE,
+          sortable: false,
+        };
+      });
+
+    return [
+      {
+        header: '구분',
+        name: 'fg',
+        width: ENUM_WIDTH.M,
+        sortable: false,
+      },
+      ...monthColumns,
+      {
+        header: '합계',
+        name: 'total',
+        width: ENUM_WIDTH.M,
+        format: 'number',
+        decimal: ENUM_DECIMAL.DEC_PRICE,
+        sortable: false,
+      },
+    ];
+  }
+
+  monthData(data) {
+    const service = new BsnProductionOrderWorkRateService('');
+    const planTotalPrice = service.calcTotal(data[0]);
+    const actualTotalPrice = service.calcTotal(data[1]);
+    const rate = (actualTotalPrice / planTotalPrice) * 100;
+
+    return [
+      { ...data[0], total: planTotalPrice },
+      { ...data[1], total: actualTotalPrice },
+      { ...data[2], total: rate },
+    ];
+  }
+
+  static monthGraphLabel() {
+    return Array(12)
+      .fill(0)
+      .map((_, i) => `${i + 1}월`);
+  }
+
+  static monthGraphData({ fg, ...rest }: { [key: string]: number }) {
+    return Object.values(rest);
+  }
+
   weekColumn(): IGridColumn[] {
     const year = this.getCastedYearForNumber();
     const month = this.getCastedMonthForNumber();
@@ -35,7 +104,7 @@ export class BsnProductionOrderWorkRateService {
       return `${week}주(${dates[0]})`;
     });
 
-    const weekColumns: IGridColumn[] = weekHeaders.map((weekHeader, i) => {
+    const weekColumns = weekHeaders.map((weekHeader, i): IGridColumn => {
       const weekKey = weeks[i] > 9 ? `${weeks[i]}` : `0${weeks[i]}`;
       return {
         header: weekHeader,
@@ -65,7 +134,100 @@ export class BsnProductionOrderWorkRateService {
     ];
   }
 
-  weekLabel() {
+  weekData(data) {
+    const planTotalPrice = this.calcTotal(data[0]);
+    const actualTotalPrice = this.calcTotal(data[1]);
+    const rate = (actualTotalPrice / planTotalPrice) * 100;
+
+    return [
+      { ...data[0], total: planTotalPrice },
+      { ...data[1], total: actualTotalPrice },
+      { ...data[2], total: rate },
+    ];
+  }
+
+  weekGraphLabel() {
     return getWeeksAtMonth(this.month).map(week => `${week}주`);
+  }
+
+  static weekGraphData({ fg, ...rest }: { [key: string]: number }) {
+    return Object.values(rest);
+  }
+
+  firstHalfDateColumn(): IGridColumn[] {
+    const dates = getRangeDateAtMonth(this.month);
+
+    const dateColumns = dates.map((date): IGridColumn => {
+      const dateKey = date > 9 ? `${date}` : `0${date}`;
+
+      return {
+        header: `${date}일`,
+        name: `${this.month}-${dateKey}`,
+        format: 'number',
+        decimal: ENUM_DECIMAL.DEC_PRICE,
+        sortable: false,
+      };
+    });
+
+    return [
+      {
+        header: '구분',
+        name: 'fg',
+        width: ENUM_WIDTH.M,
+        sortable: false,
+      },
+      ...dateColumns.slice(0, dateColumns.length / 2 + 1),
+    ];
+  }
+
+  lastHalfDateColumn(): IGridColumn[] {
+    const dates = getRangeDateAtMonth(this.month);
+
+    const dateColumns = dates.map((date): IGridColumn => {
+      return {
+        header: `${date}일`,
+        name: `${this.month}-${date}`,
+        format: 'number',
+        decimal: ENUM_DECIMAL.DEC_PRICE,
+        sortable: false,
+      };
+    });
+
+    return [
+      {
+        header: '구분',
+        name: 'fg',
+        width: ENUM_WIDTH.M,
+        sortable: false,
+      },
+      ...dateColumns.slice(dateColumns.length / 2 + 1, dateColumns.length),
+      {
+        header: '합계',
+        name: 'total',
+        width: ENUM_WIDTH.M,
+        format: 'number',
+        decimal: ENUM_DECIMAL.DEC_PRICE,
+      },
+    ];
+  }
+
+  dateData(data) {
+    const planTotalPrice = this.calcTotal(data[0]);
+    const actualTotalPrice = this.calcTotal(data[1]);
+    const rate = (actualTotalPrice / planTotalPrice) * 100;
+
+    return [
+      { ...data[0], total: planTotalPrice },
+      { ...data[1], total: actualTotalPrice },
+      { ...data[2], total: rate },
+    ];
+  }
+
+  dateGraphLabel() {
+    return getRangeDateAtMonth(this.month).map(date => `${date}일`);
+  }
+
+  static dateGraphData({ fg, ...rest }: { [key: string]: number }) {
+    return Object.values(rest);
   }
 }
