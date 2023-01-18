5e5961b4cc057f9921041311b9276ea2a9ce093d
rjh <jh.ra@kisos.net>
2023-01-18 17:07:03 +0900

    랜덤 숫자를 반환하는 함수 추가

--- src/functions/encrypt.ts
+++ src/functions/encrypt.ts
@@ -4,3 +4,10 @@ export const encryptedString = () => {
 
   return crypto.getRandomValues(unit32Array)[0].toString(16);
 };
+
+export const randomNumber = () => {
+  const crypto = window.crypto;
+  const unit32Array = new Uint32Array(1);
+
+  return crypto.getRandomValues(unit32Array)[0];
+};
