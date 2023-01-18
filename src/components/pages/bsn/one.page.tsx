5caf1f155e5ca5823ed56e1c5dfd0f229ed99a3f
rjh <jh.ra@kisos.net>
2023-01-18 12:30:04 +0900

    월 설비 가동율 주차별 미달성 유형별 분석 조회 API 추가
    차트에 월 설비 가동율 주차별 미달성 유형별 분석 데이터 연동하는 기능 추가

--- src/components/pages/bsn/one.page.tsx
+++ src/components/pages/bsn/one.page.tsx
@@ -1,9 +1,12 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { Container, Datagrid, Searchbox } from '~/components/UI';
 import { PieChart } from '~/components/UI/graph/chart-pie.ui';
-import { getToday } from '~/functions';
+import { ENUM_DECIMAL } from '~/enums';
+import { getData, getToday } from '~/functions';
+import { getWeeksAtMonth } from '~/functions/date.function';
 
 export const PgPrdBsnOne = () => {
+  const [weeklyData, setWeelkyData] = useState([]);
   return (
     <>
       <Searchbox
@@ -17,23 +20,129 @@ export const PgPrdBsnOne = () => {
         ]}
         onSearch={async ({ reg_date }: { reg_date: string }) => {
           const reg_month = reg_date.substring(0, 7);
+          const weeks = getWeeksAtMonth(reg_month);
 
-          console.log({ reg_month });
+          const weeklyEquipDownTimeTypes = await getData(
+            { reg_date: reg_month },
+            '/kpi/production/equip-downtime-type',
+          );
+
+          const weeklyEquipDownTimeTotal = weeklyEquipDownTimeTypes.reduce(
+            (acc, { fg, ...equipDownTimesForWeek }) => {
+              const totalCalculatedData = Object.entries(
+                equipDownTimesForWeek,
+              ).reduce(
+                ({ fg, total, ...datasForWeek }, [week, equipDownTime]) => {
+                  if (datasForWeek.hasOwnProperty(week))
+                    return {
+                      ...datasForWeek,
+                      [week]: datasForWeek[week] + equipDownTime,
+                      total: total + equipDownTime,
+                    };
+                  else
+                    return {
+                      ...datasForWeek,
+                      [week]: equipDownTime,
+                      total: total + equipDownTime,
+                    };
+                },
+                acc,
+              );
+
+              return { fg: '합계', ...totalCalculatedData };
+            },
+            { fg: '합계', total: 0 },
+          );
+
+          const weelkyEquipDownTimeTypesForDataGrid = [
+            ...weeklyEquipDownTimeTypes,
+            weeklyEquipDownTimeTotal,
+          ].map((weeklyEquipDownTimeType, index) => {
+            const filledWeeklyEquipDownTimeType = weeks.reduce(
+              ({ fg, total, ...datasForWeek }, cur) => {
+                if (!weeklyEquipDownTimeType.hasOwnProperty(cur))
+                  return {
+                    fg,
+                    [cur]: 0,
+                    total: total,
+                    ...datasForWeek,
+                  };
+                else
+                  return {
+                    fg,
+                    [cur]: weeklyEquipDownTimeType[cur],
+                    total: total + weeklyEquipDownTimeType[cur],
+                    ...datasForWeek,
+                  };
+              },
+              { fg: weeklyEquipDownTimeType.fg, total: 0 },
+            );
+
+            if (index < weeklyEquipDownTimeTypes.length) {
+              return {
+                ...filledWeeklyEquipDownTimeType,
+                avg:
+                  filledWeeklyEquipDownTimeType.total /
+                  weeklyEquipDownTimeTotal.total,
+              };
+            }
+
+            return { ...filledWeeklyEquipDownTimeType, avg: 0 };
+          });
+
+          setWeelkyData(weelkyEquipDownTimeTypesForDataGrid);
         }}
       />
       <div style={{ display: 'flex', flexDirection: 'row', gap: '0px 15px' }}>
         <Container style={{ width: '50%' }}>
           <Datagrid
-            data={[{}, {}, {}, {}, {}, {}, {}, {}]}
+            data={weeklyData.map(data => {
+              return Object.entries(data).reduce((acc, [key, value]) => {
+                return {
+                  ...acc,
+                  [key]: `${value}`,
+                };
+              }, {});
+            })}
             columns={[
-              { header: '원인항목' },
-              { header: '1주차' },
-              { header: '2주차' },
-              { header: '3주차' },
-              { header: '4주차' },
-              { header: '5주차' },
-              { header: '합계' },
-              { header: '점유율' },
+              { header: '원인항목', name: 'fg' },
+              {
+                header: '1주차',
+                name: '1',
+                format: 'number',
+                decimal: ENUM_DECIMAL.DEC_STCOK,
+              },
+              {
+                header: '2주차',
+                name: '2',
+                format: 'number',
+                decimal: ENUM_DECIMAL.DEC_STCOK,
+              },
+              {
+                header: '3주차',
+                name: '3',
+                format: 'number',
+                decimal: ENUM_DECIMAL.DEC_STCOK,
+              },
+              {
+                header: '4주차',
+                name: '4',
+                format: 'number',
+                decimal: ENUM_DECIMAL.DEC_STCOK,
+              },
+              {
+                header: '5주차',
+                name: '5',
+                format: 'number',
+                decimal: ENUM_DECIMAL.DEC_STCOK,
+              },
+              {
+                header: '합계',
+                name: 'total',
+                format: 'number',
+                decimal: ENUM_DECIMAL.DEC_STCOK,
+              },
+              { header: '점유율', name: 'avg', format: 'percent' },
             ]}
             disabledAutoDateColumn={true}
           />
@@ -48,7 +157,38 @@ export const PgPrdBsnOne = () => {
             borderColor: '#ffffff',
           }}
         >
-          <PieChart />
+          <PieChart
+            data={{
+              labels: weeklyData
+                .filter(({ avg }) => avg > 0)
+                .map(({ fg }) => fg),
+              datasets: [
+                {
+                  label: 'hello',
+                  data: weeklyData
+                    .filter(({ avg }) => avg > 0)
+                    .map(({ avg }) => avg * 100),
+                  backgroundColor: [
+                    'rgba(255, 99, 132, 0.2)',
+                    'rgba(54, 162, 235, 0.2)',
+                    'rgba(255, 206, 86, 0.2)',
+                    'rgba(75, 192, 192, 0.2)',
+                    'rgba(153, 102, 255, 0.2)',
+                    'rgba(255, 159, 64, 0.2)',
+                  ],
+                  borderColor: [
+                    'rgba(255, 99, 132, 1)',
+                    'rgba(54, 162, 235, 1)',
+                    'rgba(255, 206, 86, 1)',
+                    'rgba(75, 192, 192, 1)',
+                    'rgba(153, 102, 255, 1)',
+                    'rgba(255, 159, 64, 1)',
+                  ],
+                  borderWidth: 1,
+                },
+              ],
+            }}
+          />
         </div>
       </div>
       <Container style={{ minHeight: '150px' }}>textarea</Container>
