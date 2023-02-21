6ebe6a02f67144a0ff103e66eff7106030a4b6b9
rjh <jh.ra@kisos.net>
2023-02-21 11:05:00 +0900

    입하 커스텀 훅 추가

--- src/components/pages/mat/receive/receive.page.tsx
+++ src/components/pages/mat/receive/receive.page.tsx
@@ -1,89 +1,28 @@
-import React, { useRef, useState } from 'react';
-import { ReceiveHeader, ReceiveRemoteStore } from '~/apis/mat/receive';
-import { Button, Datagrid, Flexbox, Modal } from '~/components/UI';
-import { getToday } from '~/functions';
-import { isNil } from '~/helper/common';
-import { MatReceiveService } from '~/service/mat/ReceiveService';
+import React from 'react';
+import { ReceiveRemoteStoreInstance } from '~/apis/mat/receive';
+import { Flexbox } from '~/components/UI';
+import {
+  useMatReceiveModalServiceImpl,
+  useMatReceiveService,
+} from '~/service/mat/ReceiveService';
 import { MatReceiveAside } from './receive-aside';
 import { MatReceiveContent } from './receive-content';
-import { MatReceiveEditableForm } from './receive-editable-form';
 import { MatReceiveHeader } from './receive-header';
-
-const matReceiveService = new MatReceiveService();
-const matReceiveRemoteStore = new ReceiveRemoteStore();
-
-const useMatReceiveHeaderService = () => {
-  const [modalVisible, setModalVisible] = useState<boolean>(false);
-  const [formValues, setFormValues] = useState<ReceiveHeader>({
-    ...matReceiveService.empty(),
-    reg_date: getToday(),
-  });
-
-  const toggle = () => {
-    setModalVisible(!modalVisible);
-  };
-
-  return {
-    modalVisible,
-    toggle,
-    formValues,
-    setFormValues,
-  };
-};
+import { MatReceiveModal } from './receive-modal';
 
 export const PgMatReceive = () => {
-  const service = useMatReceiveHeaderService();
-  const asideGridRef = useRef(null);
-  const contentGridRef = useRef(null);
-  const [asideGridData, setAsideGridData] = useState<any[]>([]);
-  const [contentFormValues, setContentFormValues] = useState<ReceiveHeader>(
-    matReceiveService.empty(),
-  );
-
-  const asideGridClick = async e => {
-    if (!isNil(e.rowKey)) {
-      const contentGrid = contentGridRef.current.getInstance();
-      const selectedRow = e.instance.getRow(e.rowKey);
-      const contents = await matReceiveRemoteStore.getDetail(
-        selectedRow.receive_uuid,
-      );
-
-      if (contents.length > 0) {
-        setContentFormValues(contents[0].header);
-        contentGrid.resetData(contents[0].details);
-      }
-    }
-  };
+  const matReceiveRemoteStore = new ReceiveRemoteStoreInstance();
+  const matReceiveService = useMatReceiveService(matReceiveRemoteStore);
+  const matReceiveModalService = useMatReceiveModalServiceImpl();
 
   return (
     <>
-      <MatReceiveHeader service={service} />
+      <MatReceiveHeader modalService={matReceiveModalService} />
       <Flexbox>
-        <MatReceiveAside
-          gridRef={asideGridRef}
-          onGridClick={asideGridClick}
-          gridData={asideGridData}
-          setGridData={setAsideGridData}
-          remoteStore={matReceiveRemoteStore}
-        />
-        <MatReceiveContent
-          gridRef={contentGridRef}
-          formValues={contentFormValues}
-          service={matReceiveService}
-        />
+        <MatReceiveAside service={matReceiveService} />
+        <MatReceiveContent service={matReceiveService} />
       </Flexbox>
-      <Modal
-        title="입하 신규 항목 추가"
-        visible={service.modalVisible}
-        okText="저장하기"
-        onCancel={service.toggle}
-      >
-        <MatReceiveEditableForm service={service} />
-        <Button>발주 불러오기</Button>
-        <Button>행추가</Button>
-        <Button>행취소</Button>
-        <Datagrid columns={[]} />
-      </Modal>
+      <MatReceiveModal modalService={matReceiveModalService} />
     </>
   );
 };
--- src/service/mat/ReceiveService.ts
+++ src/service/mat/ReceiveService.ts
@@ -1,48 +1,176 @@
+import { useState } from 'react';
 import dayjs from 'dayjs';
-import { ReceiveHeader } from '~/apis/mat/receive';
-import { isEmpty } from '~/helper/common';
+import { ReceiveHeader, ReceiveRemoteStore } from '~/apis/mat/receive';
+import { isEmpty, isNil } from '~/helper/common';
+import { getToday } from '~/functions';
+import { URL_PATH_STD } from '~/enums';
+import { message } from 'antd';
+import { TApiSettings } from '~/components/UI';
 
-export const MatReceiveService = class {
-  constructor() {}
+const getEmptyReceiveHeader = (): ReceiveHeader => ({
+  order_date: null,
+  order_stmt_no: null,
+  order_total_price: null,
+  order_total_qty: null,
+  partner_cd: '',
+  partner_nm: '',
+  partner_uuid: '',
+  receive_uuid: '',
+  reg_date: '',
+  remark: null,
+  stmt_no: '',
+  supplier_cd: null,
+  supplier_nm: null,
+  supplier_uuid: null,
+  total_price: '',
+  total_qty: '',
+  factory_cd: '',
+  factory_nm: '',
+  factory_uuid: '',
+  created_at: '',
+  created_nm: '',
+  updated_at: '',
+  updated_nm: '',
+});
 
-  empty(): ReceiveHeader {
-    return {
-      order_date: null,
-      order_stmt_no: null,
-      order_total_price: null,
-      order_total_qty: null,
-      partner_cd: '',
-      partner_nm: '',
-      partner_uuid: '',
-      receive_uuid: '',
-      reg_date: '',
-      remark: null,
-      stmt_no: '',
-      supplier_cd: null,
-      supplier_nm: null,
-      supplier_uuid: null,
-      total_price: '',
-      total_qty: '',
-      factory_cd: '',
-      factory_nm: '',
-      factory_uuid: '',
-      created_at: '',
-      created_nm: '',
-      updated_at: '',
-      updated_nm: '',
-    };
-  }
+export const useMatReceiveService = (
+  matReceiveRemoteStore: ReceiveRemoteStore,
+) => {
+  const [receiveHeaderGridData, setHeaderGridData] = useState<ReceiveHeader[]>(
+    [],
+  );
+  const [receiveContentFormData, setContentFormData] = useState<ReceiveHeader>(
+    getEmptyReceiveHeader(),
+  );
+  const [receiveContentGridData, setContentGridData] = useState<
+    ReceiveHeader[]
+  >([]);
 
-  getHeader(header: ReceiveHeader): ReceiveHeader {
-    if (isEmpty(header.stmt_no)) {
-      return this.empty();
+  const asideGridClick = async (e: {
+    rowKey: null | number;
+    instance: { getRow: (idx: number) => ReceiveHeader };
+  }) => {
+    if (!isNil(e.rowKey)) {
+      const selectedRow = e.instance.getRow(e.rowKey);
+      const receiveContents = await matReceiveRemoteStore.getDetail(
+        selectedRow.receive_uuid,
+      );
+
+      if (receiveContents.length > 0) {
+        setContentFormData(receiveContents[0].header);
+        setContentGridData(receiveContents[0].details);
+      }
+    }
+  };
+
+  const getContentFormValues = (): ReceiveHeader => {
+    if (isEmpty(receiveContentFormData.stmt_no)) {
+      return getEmptyReceiveHeader();
     }
 
     return {
-      ...header,
-      reg_date: dayjs(header.reg_date).format('YYYY-MM-DD'),
-      total_qty: Number(header.total_qty).toFixed(0),
-      total_price: Number(header.total_price).toFixed(0),
+      ...receiveContentFormData,
+      reg_date: dayjs(receiveContentFormData.reg_date).format('YYYY-MM-DD'),
+      total_qty: Number(receiveContentFormData.total_qty).toFixed(0),
+      total_price: Number(receiveContentFormData.total_price).toFixed(0),
     };
-  }
+  };
+
+  const searchReceiveHeader = async ({ date_range }) => {
+    const header = await matReceiveRemoteStore.getHeader(
+      date_range[0].format('YYYY-MM-DD'),
+      date_range[1].format('YYYY-MM-DD'),
+    );
+
+    setHeaderGridData(header);
+    setContentFormData(getEmptyReceiveHeader());
+    setContentGridData([]);
+  };
+
+  return {
+    receiveHeaderGridData,
+    receiveContentGridData,
+    getContentFormValues,
+    searchReceiveHeader,
+    asideGridClick,
+  };
+};
+
+type Partner = { partner_uuid: string; partner_nm: string };
+type Supplier = { supplier_uuid: string; supplier_nm: string };
+
+export interface MatReceiveModalService {
+  title: string;
+  modalVisible: boolean;
+  subModalVisible: boolean;
+  formValues: ReceiveHeader;
+  openCreateReceive: () => void;
+  setPartner: (partner: Partner) => void; // (partner: { partner_uuid: string; partner_nm: string }
+  setSupplier: (supplier: Supplier) => void;
+  interlockSupplier: (ev: any) => TApiSettings;
+  subModalToggle: () => void;
+  setFormValues: () => void;
+  close: () => void;
+}
+
+export const useMatReceiveModalServiceImpl = (): MatReceiveModalService => {
+  const [title, setTitle] = useState<string>('');
+  const [modalVisible, setModalVisible] = useState<boolean>(false);
+  const [subModalVisible, setSubModalVisible] = useState<boolean>(false);
+  const [formValues, setFormValues] = useState<ReceiveHeader>(
+    getEmptyReceiveHeader(),
+  );
+
+  const openCreateReceive = () => {
+    setTitle('입하 신규 항목 추가');
+    setFormValues({ ...getEmptyReceiveHeader(), reg_date: getToday() });
+    setModalVisible(true);
+  };
+
+  const setPartner = ({ partner_uuid, partner_nm }: Partner) => {
+    setFormValues({ ...formValues, partner_uuid, partner_nm });
+  };
+
+  const setSupplier = ({ supplier_uuid, supplier_nm }: Supplier) => {
+    setFormValues({ ...formValues, supplier_uuid, supplier_nm });
+  };
+
+  const interlockSupplier = (_ev: any): TApiSettings => {
+    return {
+      uriPath: URL_PATH_STD.SUPPLIER.GET.SUPPLIERS,
+      params: {
+        partner_uuid: formValues.partner_uuid,
+      },
+      onInterlock: () => {
+        if (formValues.partner_uuid) {
+          return true;
+        } else {
+          message.warning('거래처를 먼저 선택해주세요.');
+          return false;
+        }
+      },
+    };
+  };
+
+  const subModalToggle = () => {
+    setSubModalVisible(!subModalVisible);
+  };
+
+  const close = () => {
+    setModalVisible(false);
+  };
+
+  return {
+    title,
+    modalVisible,
+    subModalVisible,
+    openCreateReceive,
+    setPartner,
+    setSupplier,
+    interlockSupplier,
+    subModalToggle,
+    formValues,
+    setFormValues,
+    close,
+  };
 };
