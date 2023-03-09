b94a20e5106c04d86ce5c887a996c889996434fb
rjh <jh.ra@kisos.net>
2023-03-09 11:26:52 +0900

    읽기 전용 텍스트 필드 추가

---
+++ src/components/UI/textbox/FormikTextBox.tsx
@@ -0,0 +1,10 @@
+import { Input } from 'formik-antd';
+import styled from 'styled-components';
+import Fonts from '~styles/font.style.module.scss';
+import Sizes from '~styles/size.style.module.scss';
+
+export const TextBox = styled(Input)`
+  width: 221px;
+  height: ${Sizes.height_datepicker_default};
+  font-size: ${Fonts.fontSize_datepicker};
+`;
