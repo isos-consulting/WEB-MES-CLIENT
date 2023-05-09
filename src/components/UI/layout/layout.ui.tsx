ba6216212dcfb57c3b7fc9814389b354032f6353
rjh <jh.ra@kisos.net>
2023-05-09 18:00:51 +0900

    정의되지 않은 속성 제거

--- src/components/UI/layout/layout.ui.tsx
+++ src/components/UI/layout/layout.ui.tsx
@@ -76,7 +76,6 @@ export const Layout: React.FC<Props> = ({ children }) => {
         </ScContent>
 
         <Footer
-          id="main-footer"
           height={layoutState.bottomSpacing}
           left={layoutState.leftSpacing}
         />
