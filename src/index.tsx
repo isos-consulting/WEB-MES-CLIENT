d0f6eba93c81f450fe564ec96d7bfb7f63c3cd2d
IDKNWHORU <junho9341@gmail.com>
2023-07-02 20:54:06 +0900

    koKR 정의 되어있는 코드를 리팩터링

--- src/index.tsx
+++ src/index.tsx
@@ -2,7 +2,7 @@ import React, { lazy, Suspense } from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 import 'antd/dist/antd.css';
-import koKr from 'antd/lib/locale-provider/ko_KR';
+import koKr from './v2/langs/kr.lang';
 import 'dayjs/locale/ko';
 import { GlobalStyles } from './styles/theme.style';
 
@@ -13,22 +13,6 @@ const RecoilRoot = lazy(() =>
 const ConfigProvider = lazy(() =>
   import('antd').then(module => ({ default: module.ConfigProvider })),
 );
-
-koKr.DatePicker.lang.shortWeekDays = ['일', '월', '화', '수', '목', '금', '토'];
-koKr.DatePicker.lang.shortMonths = [
-  '1월',
-  '2월',
-  '3월',
-  '4월',
-  '5월',
-  '6월',
-  '7월',
-  '8월',
-  '9월',
-  '10월',
-  '11월',
-  '12월',
-];
 
 ReactDOM.render(
   <Suspense fallback="...loading">
---
+++ src/v2/langs/kr.lang.ts
@@ -0,0 +1,19 @@
+import koKR from 'antd/lib/locale-provider/ko_KR';
+
+koKR.DatePicker.lang.shortWeekDays = ['일', '월', '화', '수', '목', '금', '토'];
+koKR.DatePicker.lang.shortMonths = [
+  '1월',
+  '2월',
+  '3월',
+  '4월',
+  '5월',
+  '6월',
+  '7월',
+  '8월',
+  '9월',
+  '10월',
+  '11월',
+  '12월',
+];
+
+export default koKR;
